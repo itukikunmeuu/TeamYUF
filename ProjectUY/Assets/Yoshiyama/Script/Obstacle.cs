edac4080c56b91db41a7a305cd03d86e67c36b47
renka1225 <lotus1225r@gmail.com>
2025-04-09 16:50:36 +0900

    up

--- ProjectUY/Assets/Yoshiyama/Script/Obstacle.cs
+++ ProjectUY/Assets/Yoshiyama/Script/Obstacle.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    public int value = 0;
+    public int leftValue = 0;
+    public int rightValue = 0;
+    private int value = 0;
+    Collider other;
 
     private void OnTriggerEnter(Collider other)
     {
+        // プレイヤーのスクリプトを取得
+        PlayerScript player = other.GetComponent<PlayerScript>();
+        if (player == null) return;
+
+        // プレイヤーが当たった時
         if (other.CompareTag("Player"))
         {
-            //いったん仮置き、タグをつけて判定します
-            //PlayerCounter counter = other.GetComponent<PlayerCounter>();
-            //if (counter != null)
-            //{
-            //    counter.AddCount(value);
-            //    Destroy(gameObject);
-            //}
+            // プレイヤー左側
+            if(other.transform.position.x < 0.0f)
+            {
+                value = leftValue;
+            }
+            // 右側
+            else
+            {
+                value = rightValue;
+            }
+            
+            Debug.Log("value : " + value);
+            player.AddCount(value);
         }
     }
 }
